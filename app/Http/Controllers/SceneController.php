24a6c6a5bb9bc5774ffbc864b2056b88d489a774
Kobayashi139 <kobayashi.shi.00@gmail.com>
2023-12-02 22:18:23 +0900

    create index

--- app/Http/Controllers/SceneController.php
+++ app/Http/Controllers/SceneController.php
@@ -10,5 +10,9 @@ class SceneController extends Controller
     public function index(Scene $scene)
     {
         return $scene->get(); //$sceneの中身を戻り値にする 
-    }//
+    }
+
+    public function create(Request $request){
+        return view('scene.create')->with(['name' => $name ,'reviews' => $review->get()]);
+    }
 }
--- database/migrations/2023_12_02_175640_create_scenes_table.php
+++ database/migrations/2023_12_02_175640_create_scenes_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('scenes', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 80);
+            $table->string('content', 200);
+            $table->tinyInteger('created_by');
+            $table->tinyInteger('update_by');
             $table->timestamps();
         });
     }
