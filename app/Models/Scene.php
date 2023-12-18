752f6639b7d79431ee54dc1cb28bc488f0c55779
Kobayashi139 <kobayashi.shi.00@gmail.com>
2023-12-18 23:14:12 +0900

    add.pagination

--- app/Models/Scene.php
+++ app/Models/Scene.php
@@ -14,12 +14,7 @@ class Scene extends Model
         'content',
         ];
     
-    public function getByLimit(int $limit_count = 10)
-    {
-        return $this->orderBy('updated_at' ,'DESC')->limit($limit_count)->get();
-    }
-    
-    public function getPaginateByLimit(int $limit_count =10 )
+    public function getPaginateByLimit(int $limit_count =5 )
     {
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
